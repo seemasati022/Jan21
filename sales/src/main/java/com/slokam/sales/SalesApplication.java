8ef8a120f586fa8b312aa6095d39746d495253cb
seemasati022 <82013091+seemasati022@users.noreply.github.com>
2021-04-14 21:30:47 +0530

    second commit

---
+++ sales/src/main/java/com/slokam/sales/SalesApplication.java
@@ -0,0 +1,13 @@
+package com.slokam.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApplication.class, args);
+	}
+
+}
---
+++ sales/src/test/java/com/slokam/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slokam.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
